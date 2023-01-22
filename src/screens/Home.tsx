2a3e7641d42785c068031de7760564f4a830b9f6
Saymon Damásio <saymon.damasio95@gmail.com>
2023-01-22 17:57:51 -0300

    feat: finish page home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { ScrollView, Text, View } from "react-native";
 import { DAY_SIZE, HabitDay } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -11,6 +12,8 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -33,7 +36,10 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toString()} />
+            <HabitDay
+              key={date.toString()}
+              onPress={() => navigate("habit", { date: date.toISOString() })}
+            />
           ))}
           {Array.from({ length: amountOfDaysToFill }).map((_, i) => (
             <View
