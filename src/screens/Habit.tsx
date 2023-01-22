3e8872f9bd897b40f68d24ecc3a17b1599cb531f
Saymon Damásio <saymon.damasio95@gmail.com>
2023-01-22 17:57:43 -0300

    feat: finish page habit

--- src/screens/Habit.tsx
+++ src/screens/Habit.tsx
@@ -1,5 +1,45 @@
-import { View } from "react-native";
+import { RouteProp, useRoute } from "@react-navigation/native";
+import dayjs from "dayjs";
+import { ScrollView, Text, View } from "react-native";
+import { BackButton } from "../components/BackButton";
+import { Checkbox } from "../components/Checkbox";
+import { ProgressBar } from "../components/ProgressBar";
+
+type HabitParams = {
+  date: string;
+};
 
 export function Habit() {
-  return <View className="flex-1 bg-background px-8 pt-16"></View>;
+  const route = useRoute();
+  const { date } = route.params as HabitParams;
+
+  const parsedDate = dayjs(date);
+  const dayOfWeek = parsedDate.format("dddd");
+  const dayAndMonth = parsedDate.format("DD/MM");
+
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
+          {dayOfWeek}
+        </Text>
+
+        <Text className="text-white font-extrabold text-3xl">
+          {dayAndMonth}
+        </Text>
+
+        <ProgressBar progress={30} />
+
+        <View className="mt-3">
+          <Checkbox label="Beber água" checked />
+          <Checkbox label="Fazer exercício" />
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
